9e97f1ade9a73dba9d40dca16c3ce68fa8edc61b
Shivam Agarwal <99248722+shivamagarwal2510@users.noreply.github.com>
2023-11-28 18:01:06 +0530

    back to api

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -1,49 +1,48 @@
 import React, { useState } from "react";
 import axios from "axios";
-import data from "../Design1";
+// import data from "../Design1";
 const App: React.FC = () => {
-  // const [data, setData] = useState([]);
   const [companyType, setCompanyType] = useState("");
   const [companyName, setCompanyName] = useState("");
   const [loading, setLoading] = useState(false);
 
   const handleGenerate = () => {
-    parent.postMessage({ pluginMessage: { type: "generate", data } }, "*");
-    // const fetchData = async () => {
-    //   try {
-    //     setLoading(true);
-    //     const { data } = await axios.post(
-    //       "https://prompt2ui-rn6x.onrender.com/post",
-    //       {
-    //         variable1: companyType,
-    //         variable2: companyName,
-    //       },
-    //       {
-    //         headers: {
-    //           "Content-Type": "application/json",
-    //         },
-    //       }
-    //     );
-    //     console.log("--data--", data);
-    //     try {
-    //       const { data } = await axios.get(
-    //         "https://prompt2ui-rn6x.onrender.com/get_data"
-    //       );
-    //       console.log("-- get data--", data);
-    //       parent.postMessage(
-    //         { pluginMessage: { type: "generate", data } },
-    //         "*"
-    //       );
-    //     } catch (error) {
-    //       console.log("--get error--", error);
-    //     }
-    //   } catch (error) {
-    //     console.log("--error--", error);
-    //   } finally {
-    //     setLoading(false);
-    //   }
-    // };
-    // fetchData();
+    // parent.postMessage({ pluginMessage: { type: "generate", data } }, "*");
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.post(
+          "https://prompt2ui-rn6x.onrender.com/post",
+          {
+            variable1: companyType,
+            variable2: companyName,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        console.log("--data--", data);
+        try {
+          const { data } = await axios.get(
+            "https://prompt2ui-rn6x.onrender.com/get_data"
+          );
+          console.log("-- get data--", data);
+          parent.postMessage(
+            { pluginMessage: { type: "generate", data } },
+            "*"
+          );
+        } catch (error) {
+          console.log("--get error--", error);
+        }
+      } catch (error) {
+        console.log("--error--", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
   };
   const handleCompanyType = (e: any) => {
     setCompanyType(e.target.value);
